953777cb399afca036b76ebcc8cc8e7828fa2d27
nkoubamarius <nkoubamarius@gmail.com>
2020-09-16 03:00:00 +0100

    Building Hulu clone with reactjs and FlipMode

---
+++ src/VideoCard.js
@@ -0,0 +1,27 @@
+import { ThumbUpSharp } from '@material-ui/icons';
+import React, {forwardRef} from 'react'
+import TextTruncate from 'react-text-truncate';
+import './VideoCard.css'
+
+const base_url="https://image.tmdb.org/t/p/original/";
+
+const  VideoCard=forwardRef(({movie}, ref) =>{
+    return (
+        <div ref={ref} className="videoCard">
+            <img src={`${base_url}${movie.backdrop_path  || movie.poster_path}`} alt="Movie poster"/>
+            <TextTruncate
+                line={1}
+                element="p"
+                textTruncateChild={<a href="#">Read on</a>}
+                text={movie.overview}
+            />
+            <h2>{movie.title || movie.original_name}</h2>
+            <p className="videoCard__stats">
+                {movie.media_type && `${movie.media_type} *`}
+                {movie.release_date || movie.first_air_date} -
+                <ThumbUpSharp/> {" "}{movie.vote_count}</p>
+        </div>
+    )
+})
+
+export default VideoCard
